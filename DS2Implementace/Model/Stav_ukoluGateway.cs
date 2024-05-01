2b495a15402a2b3ce4cc99f58ec8503df02f84b5
Michal Duba <m.duba.work@gmail.com>
2024-05-01 13:07:29 +0200

    Project fixed and done

---
+++ DS2Implementace/Model/Stav_ukoluGateway.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS2Implementace.Model
+{
+    internal class Stav_ukoluGateway
+    {
+        public Stav_ukolu GetOne(SqlConnection connection, int id)
+        {
+            Stav_ukolu result = new Stav_ukolu();
+            string commandString = "Select * FROM Stav_ukolu WHERE stav_ukolu_id = @id";
+            using(SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = commandString;
+                command.Parameters.AddWithValue("id", id);
+                var reader = command.ExecuteReader();
+                reader.Read();
+                result.Stav_ukolu_id = (int)reader[0];
+                result.Nazev = (string)reader[1];
+                result.Popis = (string)reader[2];
+                result.Datum_vytvoreni = (DateTime)reader[3];
+                result.Posledni_zmena = (DateTime)reader[4];
+            }
+            return result;
+        }
+        public List<Stav_ukolu> GetAll(SqlConnection connection)
+        {
+            List<Stav_ukolu> list = new List<Stav_ukolu>();
+            string commandString = "Select * From Stav_ukolu";
+            using(SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = commandString;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Stav_ukolu result = new Stav_ukolu();
+                        result.Stav_ukolu_id = (int)reader[0];
+                        result.Nazev = (string)reader[1];
+                        result.Popis = (string)reader[2];
+                        result.Datum_vytvoreni = (DateTime)reader[3];
+                        result.Posledni_zmena = (DateTime)reader[4];
+                        list.Add(result);
+                    }
+                }
+            }
+            return list;
+        }
+        public int GetIDByNazev(SqlConnection connection, string nazev)
+        {
+            List<Stav_ukolu> list = GetAll(connection);
+            foreach(Stav_ukolu record in list)
+            {
+                if(record.Nazev == nazev)
+                {
+                    return record.Stav_ukolu_id;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ DS2Implementace/Model/Typ_projektuGateway.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS2Implementace.Model
+{
+    internal class Typ_projektuGateway
+    {
+        public List<Typ_projektu> GetAll(SqlConnection connection)
+        {
+            List<Typ_projektu> list  = new List<Typ_projektu>();
+            string commandString = "Select * from typ_projektu";
+            using(SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = commandString;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Typ_projektu result = new Typ_projektu();
+                        result.Typ_projektu_id = reader.GetInt32(0);
+                        result.Nazev = reader.GetString(1);
+                        result.Datum_vytvoreni = reader.GetDateTime(2);
+                        result.Posledni_zmena = reader.GetDateTime(3);
+                        list.Add(result);
+                    }
+                }
+            }
+            return list;
+        }
+
+        public int GetIDByNazev(SqlConnection connection,  string nazev) 
+        {
+            List<Typ_projektu> list = GetAll(connection);
+            foreach(Typ_projektu record in list)
+            {
+                if(record.Nazev == nazev)
+                {
+                    return record.Typ_projektu_id;
+                }
+            }
+            return -1;
+        }
+    }
+}
